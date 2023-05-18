7f866f80db2ff628c68a558b229dac88a000e3b2
StergiouGiorgos <127401068+StergiouGiorgos@users.noreply.github.com>
2023-05-18 13:55:11 +0300

    Add files via upload

---
+++ Deadline5/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    public String name;
+    public String email;
+    public String phone;
+
+    public String address;
+    public String password;
+
+    public String id;
+}
