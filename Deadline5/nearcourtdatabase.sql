fb12387af716d4d1dce1faca8657c48bef506904
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2023-05-20 17:11:56 +0300

    Update nearcourtdatabase.sql

--- Deadline5/nearcourtdatabase.sql
+++ Deadline5/nearcourtdatabase.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 20, 2023 at 02:27 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Εξυπηρετητής: 127.0.0.1
+-- Χρόνος δημιουργίας: 16 Μάη 2023 στις 19:43:00
+-- Έκδοση διακομιστή: 10.4.27-MariaDB
+-- Έκδοση PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,91 +18,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `nearcourtdatabase`
+-- Βάση δεδομένων: `nearcourtdatabase`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rates`
+-- Δομή πίνακα για τον πίνακα `rates`
 --
 
 CREATE TABLE `rates` (
-  `rate` varchar(40) NOT NULL,
-  `stars` int(2) NOT NULL,
-  `rate_id` int(20) NOT NULL,
-  `user_id` int(10) NOT NULL
+                         `user_id` int(10) NOT NULL,
+                         `rate` varchar(220) NOT NULL,
+                         `stars` enum('0.5','1','1.5','2','2.5','3','3.5','4','4.5','5') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `rates`
+-- Άδειασμα δεδομένων του πίνακα `rates`
 --
 
-INSERT INTO `rates` (`rate`, `stars`, `rate_id`, `user_id`) VALUES
-('malakas', 5, 1, 1),
-('sfafdasfd', 6, 2, 1),
-('kalos kalos', 5, 3, 2),
-('gamaei', 4, 4, 2),
-('kalos kalosklkl', 6, 5, 2),
-('gamaei full', 2, 6, 2),
-('kalos kalosopopo', 3, 7, 2),
-('gamaeighghjjg', 6, 8, 2);
+INSERT INTO `rates` (`user_id`, `rate`, `stars`) VALUES
+                                                     (1, 'kalos malakas einai', '2'),
+                                                     (2, 'megalos malakas respect', '4.5'),
+                                                     (1, 'kalos', '3.5');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Δομή πίνακα για τον πίνακα `users`
 --
 
 CREATE TABLE `users` (
-  `user_id` int(10) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `phone` varchar(200) DEFAULT NULL,
-  `address` varchar(200) DEFAULT NULL,
-  `password` varchar(200) NOT NULL
+                         `user_id` int(10) NOT NULL,
+                         `email` varchar(40) NOT NULL,
+                         `username` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Άδειασμα δεδομένων του πίνακα `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `email`, `phone`, `address`, `password`) VALUES
-(1, 'giio', 'stergos@gmail.com', '6969696969', 'pipa2', 'stergos'),
-(2, 'malakas', 'giannissk@gmail.com', '696969696969', '5353', 'giannis');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `rates`
---
-ALTER TABLE `rates`
-  ADD PRIMARY KEY (`rate_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `rates`
---
-ALTER TABLE `rates`
-  MODIFY `rate_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+INSERT INTO `users` (`user_id`, `email`, `username`) VALUES
+                                                         (1, 'kapa@gmail.com', 'malakas'),
+                                                         (2, 'kapa2@gmail.com', 'malakas2');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
