98f00caec8fa9e4742bd89cb55702bfcdbed86cd
Saridakis Georgios <127314790+Saridak@users.noreply.github.com>
2023-05-22 20:05:41 +0300

    Add files via upload

---
+++ Deadline5/groupp.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Εξυπηρετητής: 127.0.0.1
+-- Χρόνος δημιουργίας: 22 Μάη 2023 στις 19:05:19
+-- Έκδοση διακομιστή: 10.4.27-MariaDB
+-- Έκδοση PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Βάση δεδομένων: `nearcourtdatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `groupp`
+--
+
+CREATE TABLE `groupp` (
+  `type` enum('private','public') NOT NULL,
+  `sport` enum('Football','Basketball','Tennis') NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `number_of_players` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
